bfb8d0bea9e10073cdc945a538218360fd26f589
bhoff <bruce.hoff@sagebase.org>
2020-04-23 07:40:50 -0700

    SC-125

--- src/test/java/synapseawsconsolelogin/AuthTest.java
+++ src/test/java/synapseawsconsolelogin/AuthTest.java
@@ -2,7 +2,7 @@ package synapseawsconsolelogin;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.Map;
@@ -10,7 +10,9 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.junit.After;
 import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -29,15 +31,32 @@ import com.amazonaws.services.simplesystemsmanagement.model.PutParameterRequest;
 @RunWith(MockitoJUnitRunner.class)
 public class AuthTest {
 	
+	private static final String TEST_PROPERTY_NAME = "testPropertyName";
+
 	@Mock
 	private HttpServletRequest req;
 	
-	@Test
-	public void testReadTeamToArnMap() {
+	@Mock
+	private HttpGetExecutor mockHttpGetExecutor;
+	
+	@Before
+	public void before() {
 		System.setProperty("TEAM_TO_ROLE_ARN_MAP","[{\"teamId\":\"123456\",\"roleArn\":\"arn:aws:iam::foo\"},{\"teamId\":\"345678\",\"roleArn\":\"arn:aws:iam::bar\"}]");
 		System.setProperty("AWS_REGION", "us-east-1");
 		System.setProperty("USER_CLAIMS", "userid,user_name");
+		System.setProperty(Auth.PROPERTIES_FILENAME_PARAMETER, "test.properties");
+	}
 	
+	@After
+	public void after() {
+		System.clearProperty("TEAM_TO_ROLE_ARN_MAP");
+		System.clearProperty("AWS_REGION");
+		System.clearProperty("USER_CLAIMS");
+		System.clearProperty(TEST_PROPERTY_NAME);
+	}
+	
+	@Test
+	public void testReadTeamToArnMap() {
 		Auth auth = new Auth();
 		
 		Map<String,String> map = auth.getTeamToRoleMap();
@@ -49,10 +68,6 @@ public class AuthTest {
 	
 	@Test
 	public void testGetAuthUrl() {
-		System.setProperty("TEAM_TO_ROLE_ARN_MAP","[{\"teamId\":\"123456\",\"roleArn\":\"arn:aws:iam::foo\"},{\"teamId\":\"345678\",\"roleArn\":\"arn:aws:iam::bar\"}]");
-		System.setProperty("AWS_REGION", "us-east-1");
-		System.setProperty("USER_CLAIMS", "userid,user_name");
-		
 		Auth auth = new Auth();
 		
 		String expected = "https://signin.synapse.org?response_type=code&client_id=%s&redirect_uri=%s&claims={\"id_token\":{\"team\":{\"values\":[\"123456\",\"345678\"]},\"userid\":{\"essential\":true},\"user_name\":{\"essential\":true}},\"userinfo\":{\"team\":{\"values\":[\"123456\",\"345678\"]},\"userid\":{\"essential\":true},\"user_name\":{\"essential\":true}}}";
@@ -62,29 +77,25 @@ public class AuthTest {
 	
 	@Test
 	public void testGetPropertyFromGlobalPropertiesFile() {
-		String name = "testPropertyName";
-		String value = "someOtherValue";
+		String value = "testPropertyValue";
 		Auth auth = new Auth();
 		
-		assertEquals(value, auth.getProperty(name));
+		assertEquals(value, auth.getProperty(TEST_PROPERTY_NAME));
 		
 	}
-
+	
 	@Test
 	public void testGetPropertyOverridingFileWithProperty() {
-		String name = "testPropertyName";
 		String value = "someOtherValue";
-		System.setProperty(name, value);
+		System.setProperty(TEST_PROPERTY_NAME, value);
 		Auth auth = new Auth();
-		assertEquals(value, auth.getProperty(name));
-		
+		assertEquals(value, auth.getProperty(TEST_PROPERTY_NAME));
 	}
 
 	@Test
 	public void testGetMissingOptionalProperty() {
 		Auth auth = new Auth();
 		assertNull(auth.getProperty("undefined-property", false));
-		
 	}
 	
 	@Test
@@ -97,7 +108,6 @@ public class AuthTest {
 			Assume.assumeNoException(e);
 		}
 		Assume.assumeNotNull(credentials, credentials.getAWSAccessKeyId(), credentials.getAWSSecretKey());
-		
 		
 		String propertyName = UUID.randomUUID().toString();
 		String propertyValue = UUID.randomUUID().toString();
@@ -130,23 +140,23 @@ public class AuthTest {
 		StringBuffer urlBuffer = new StringBuffer();
 		urlBuffer.append("https:www.foo.com/bar");
 		when(req.getRequestURL()).thenReturn(urlBuffer);
-		
 		when(req.getRequestURI()).thenReturn("/bar");
+		when(mockHttpGetExecutor.executeHttpGet(anyString())).thenReturn("{\"SigninToken\":\"token\"}");
 		
 		Credentials credentials = new Credentials();
 		credentials.setAccessKeyId("keyId");
 		credentials.setSecretAccessKey("keySecret");
 		credentials.setSessionToken("token");
 		
-		// method under test
 		Auth auth = new Auth();
-		String actual = auth.getConsoleLoginURL(req, credentials);
 		
-		String expectedPrefix = "https://signin.aws.amazon.com/federation?Action=login&SigninToken=";
-		String expectedSuffix = "&Issuer=https%3Awww.foo.com&Destination=https%3A%2F%2Fus-east-1.console.aws.amazon.com%2Fservicecatalog%2Fhome%3Fregion%3Dus-east-1%23%2Fproducts";
+		// method under test
+		String actual = auth.getConsoleLoginURL(req, credentials, mockHttpGetExecutor);
 		
-		assertTrue(actual.startsWith(expectedPrefix));
-		assertTrue(actual.endsWith(expectedSuffix));
+		String expected = "https://signin.aws.amazon.com/federation?Action=login&SigninToken=token"+
+				 "&Issuer=https%3Awww.foo.com&Destination=https%3A%2F%2Fus-east-1.console.aws.amazon.com%2Fservicecatalog%2Fhome%3Fregion%3Dus-east-1%23%2Fproducts";
+		
+		assertEquals(expected, actual);
 	}
 
 }
