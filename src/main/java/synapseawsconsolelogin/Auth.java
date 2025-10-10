62b2c6a2e4add7da9570c0a3dc8404a71ce5c68e
Khai Do <3697686+zaro0508@users.noreply.github.com>
2025-10-09 17:51:27 -0700

    [IT-4462] Update dependency (#110)
    The javax namespace is outdated and no longer supported after Java 9.
    Newer Tomcat versions are built on Jakarta EE and uses the jakarta
    namespace[1].  This migrates javax to jakarta and refactoring
    code to match where necessary.
    
    [1] https://jakarta.ee/blogs/javax-jakartaee-namespace-ecosystem-progress/

--- src/main/java/synapseawsconsolelogin/Auth.java
+++ src/main/java/synapseawsconsolelogin/Auth.java
@@ -30,10 +30,10 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
